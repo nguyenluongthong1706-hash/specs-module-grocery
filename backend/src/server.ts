8b748cfa215a319c07b863782ec5b8d37b27c6d6
thongnguyen003 <thong0855@gmail.com>
2025-12-22 21:36:33 +0700

    feature: [pnv-thong-be1-dev] manage category

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import categoryRoutes from './routes/categoryRoutes';
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/categories', categoryRoutes);
 
 
 // Route mặc định
